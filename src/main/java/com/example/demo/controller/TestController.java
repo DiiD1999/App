163b9cc5286bdb085ecc88016409361a9dc9a9f6
DiiD1999 <DiiD1999@outlook.com>
2023-02-24 22:55:27 +0800

    feat: 添加其他实现

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +15,12 @@ public class TestController {
     @GetMapping("/test")
     public ResponseEntity<Object> Get(){
         return new ResponseEntity<>("test", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Object> getUser() {
+        User user = new User("liu", 1);
+        System.out.println(user.toString());
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
